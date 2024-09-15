9cdf69e39dda1b0fbd93f242db0a067b3ab83137
KempeGowda Group Of Industries Ltd <kempegowdagroupofindustriesltd@gmail.com>
2024-09-15 12:43:06 -0400

    stock_data.py

--- .streamlit_app/stock_tracker/stock_data.py
+++ .streamlit_app/stock_tracker/stock_data.py
@@ -1,36 +1,10 @@
 import requests
 import pandas as pd
-from .utils import format_price, parse_api_response, extract_stock_info
+from .utils import parse_api_response
 
-API_KEY = '7BJZMCBGIP1SHX23'
+API_KEY = 'YOUR_ALPHAVANTAGE_API_KEY'
 BASE_URL = 'https://www.alphavantage.co/query'
 
-
-
-def get_stock_data(symbol):
-    """Fetch real-time stock data for a given symbol."""
-    params = {
-        'function': 'TIME_SERIES_INTRADAY',
-        'symbol': symbol,
-        'interval': '5min',
-        'apikey': API_KEY
-    }
-    response = requests.get(BASE_URL, params=params)
-    data, error = parse_api_response(response)
-    
-    if error:
-        return None, error
-
-    stock_info = extract_stock_info(data)
-    if stock_info:
-        stock_info['price'] = format_price(stock_info['price'])
-        return stock_info, None
-    return None, "No stock data available."
-
-import requests
-from .utils import parse_api_response  # Import parse_api_response from utils
-
-# Example usage within your function
 def search_stock(query):
     """Search for stocks based on a query."""
     params = {
@@ -50,15 +24,18 @@ def search_stock(query):
     
     return pd.DataFrame(), "No matches found."  # Return empty DataFrame and message
 
-
-def extract_stock_info(data):
-    """Extract relevant stock information from the API response data."""
-    if 'Time Series (5min)' in data:
-        latest_time = list(data['Time Series (5min)'].keys())[0]
-        info = data['Time Series (5min)'][latest_time]
-        return {
-            'price': info['4. close'],
-            'high': info['2. high'],
-            'low': info['3. low']
-        }
-    return None
+def get_stock_data(symbol):
+    """Get stock data for a given symbol."""
+    params = {
+        'function': 'TIME_SERIES_INTRADAY',
+        'symbol': symbol,
+        'interval': '5min',
+        'apikey': API_KEY
+    }
+    response = requests.get(BASE_URL, params=params)
+    data, error = parse_api_response(response)
+    
+    if error:
+        return None, error  # Return None and error
+    
+    return data, None  # Return data and no error
